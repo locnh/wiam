b8947d6f1a7cd466a2bfa71f0c84f2df0646b63e
Huu Loc Nguyen <huu_loc.nguyen@share-now.com>
2021-06-04 16:32:04 +0200

    Release v0.1.1

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o whoami .
 FROM alpine
 
 RUN apk add --no-cache wget
+RUN wget https://browscap.org/stream?q=BrowsCapINI -O browscap.ini
 
 COPY scripts/docker-entrypoint.sh /entrypoint.sh
 COPY --from=builder /app/whoami /whoami
--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 if [ $TRAVIS_BRANCH != "master" ]; then
     docker tag locnh/whoami locnh/whoami:$TRAVIS_BRANCH
